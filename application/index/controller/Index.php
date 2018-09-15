aa9457362600ca6b8824fbc7f65509729c2825da
nanpaisanshushu <zj_phper@163.com>
2018-09-15 18:17:01 +0800

    Initial commit

---
+++ application/index/controller/Index.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\index\controller;
+
+class Index
+{
+    public function index()
+    {
+    	return 'First for GitHub'
+    	return 'First for GitHub'
+    	return 'First for GitHub'
+    	return 'First for GitHub'
+    	return 'First for GitHub'
+    	return 'First for GitHub'
+    	return 'First for GitHub'
+    }
+}
